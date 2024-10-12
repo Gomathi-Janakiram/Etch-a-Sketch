341caac126227745f402f9282c175bc36361d05f
Gomathi-07 <gomathi.janakiram@banfico.com>
2024-10-12 12:15:09 +0530

    added functionalities for buttons in sidebar

--- script.js
+++ script.js
@@ -3,10 +3,38 @@ const GRID_SIZE  = 16;
 
 let currentMode = MODE;
 let currentGridSize = GRID_SIZE;
+
 let isGridClicked = false;
+let colorSelectedFromPicker = 'black';
 
 const gridSize = document.querySelector("#range_selector");
+const colorPicker = document.querySelector(".color_selector");
 const sketchBoard = document.querySelector(".sketch_board"); 
+const buttons = document.querySelectorAll("button");
+const clearButton = document.querySelector("#clear")
+
+gridSize.addEventListener("change", (e) => {
+    currentGridSize = e.target.value;
+    updateGridSize(currentGridSize)
+})
+
+colorPicker.addEventListener("change", (e) => {
+    colorSelectedFromPicker = e.target.value;
+})
+
+clearButton.addEventListener("click", () => {
+    updateGridSize(currentGridSize)
+})
+
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        buttons.forEach((btn) => btn.classList.remove("active_button"))
+        const selectedButton = e.target;
+        currentMode = selectedButton.id;
+        selectedButton.classList.add("active_button")
+    })
+})
 
 const updateGridSize = (size) => {
     const gridSizeLabel = document.querySelector("#range_label");
@@ -14,11 +42,6 @@ const updateGridSize = (size) => {
     sketchBoard.innerHTML = ''
     createGrids()
 }
-
-gridSize.addEventListener("change", (e) => {
-    currentGridSize = e.target.value;
-    updateGridSize(currentGridSize)
-})
 
 const createGrids = () => {
     for (let i = 1; i<= currentGridSize * currentGridSize; i++) {
@@ -30,19 +53,25 @@ const createGrids = () => {
 }
 
 const generateBackgoundColor = () => {
-    return Math.floor(Math.random() * 255)
+    if (currentMode == "color") {
+        return colorSelectedFromPicker
+    } else if (currentMode == "rainbow") {
+        return `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})` 
+    } else if (currentMode == "erase") {
+        return 'white'
+    }
 }
 
 sketchBoard.addEventListener("mousedown", (e) => {
     isGridClicked = true;
     const hoveredEle = e.target;
-    hoveredEle.style.backgroundColor = `rgb(${generateBackgoundColor()}, ${generateBackgoundColor()}, ${generateBackgoundColor()})`
+    hoveredEle.style.backgroundColor = generateBackgoundColor()
 })
 
 sketchBoard.addEventListener("mousemove", (e) => {
     if (isGridClicked) {
         const hoveredEle = e.target;
-        hoveredEle.style.backgroundColor = `rgb(${generateBackgoundColor()}, ${generateBackgoundColor()}, ${generateBackgoundColor()})`
+        hoveredEle.style.backgroundColor = generateBackgoundColor()
     }
 })
 
